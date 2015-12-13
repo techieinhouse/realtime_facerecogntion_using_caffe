abc5c31c64b61f4efbb32d1845b8d20a27e1fad8
yugeshwar <yugeshwarzee@gmail.com>
2015-12-13 08:01:22 +0100

    Rename gender.cpp to gender.h

---
+++ gender.h
@@ -0,0 +1,36 @@
+
+
+#include "defs.h"
+
+
+using namespace caffe;  // NOLINT(build/namespaces)
+using std::string;
+  typedef std::pair<string, float> Prediction;
+class Classifier1 {
+ public:
+  Classifier1(const string& model_file,
+             const string& trained_file,
+             const string& mean_file,
+             const string& label_file);
+
+
+std::vector<Prediction> Classify(const cv::Mat& img, int N = 1);
+
+ private:
+  void SetMean(const string& mean_file);
+
+  std::vector<float> Predict(const cv::Mat& img);
+
+  void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+  void Preprocess(const cv::Mat& img,
+                  std::vector<cv::Mat>* input_channels);
+void predictions();
+ private:
+  shared_ptr<Net<float> > net_;
+  cv::Size input_geometry_;
+  int num_channels_;
+  cv::Mat mean_;
+  std::vector<string> labels_;
+  
+};
